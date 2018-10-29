0458adb607038772d26b7045b2e20587d964c711
tatiana.mojica <tatiana.mojica@bustle.com>
2018-10-29 19:04:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+  if array.length == 2 
+   return array.join(" and ")
+  elsif array.length >= 3 
+    last_element = array.pop
+    return array.join(", ") << ", and " + last_element
+  else 
+    array.join 
+  end
+end 
 
-end
+
